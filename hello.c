99c73ad5e1d8b6f39710c20080fc2ad26d9d26d1
46ra20 <rakib2046.md@gmail.com>
2023-10-01 16:56:55 +0600

    programming c

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main(){
+    int i=5,j;
+    scanf("%d",&j);
+    printf("%d",i+j);
+}
---
+++ hi/hi/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    scanf("%d",&i);
+    printf("Hello world!\n%d",i+25);
+    return 0;
+}
