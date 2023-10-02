bab007a5ba1dab8f578917f74d98cc082884e809
46ra20 <rakib2046.md@gmail.com>
2023-10-02 12:33:39 +0600

    module 1

---
+++ Programming C/datatype.c
@@ -0,0 +1,11 @@
+// int float char
+#include<stdio.h>
+int main(){
+    int i=10;
+    float f=13.6;
+    char c='c';
+    // printf("%d %0.2f %c",i,f,c);
+    // what if ,if i change 
+    printf("%d %.2f %c", c, i, f); 
+    return 0;
+}
--- hello.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-void main(){
-    int i=5,j;
-    scanf("%d",&j);
-    printf("%d",i+j);
-}
