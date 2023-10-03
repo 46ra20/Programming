84018ec3b0327fda5f49fd0f20ec7b0ac3851ba5
46ra20 <rakib2046.md@gmail.com>
2023-10-03 11:22:52 +0600

    folder change

--- Programming C/Module_1/datatype.c
+++ Programming C/Module_1/datatype.c
@@ -3,9 +3,12 @@
 int main(){
     int i=10;
     float f=13.6;
+    int x;
+    scanf("%d", &x);
     char c='c';
     // printf("%d %0.2f %c",i,f,c);
     // what if ,if i change 
     printf("%d %.2f %c", c, i, f); 
+    printf("%d",x);
     return 0;
 }
