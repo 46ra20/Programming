58584f8e5d7855816c6ad12b777fe757865545a6
46ra20 <rakib2046.md@gmail.com>
2023-10-03 11:34:16 +0600

    module 2 operator

---
+++ Programming C/Module_2/operator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    // Arithmatic Operator, Logical Operator, Relational Operator
+    //+,-,/,*  this called is arithamatic operator
+    //<,>,<=,>=,!=,== this is relational operator
+    //&&,|| this is logical operator
+    printf("%d\n", 6 + 5);
+    printf("%d\n", 6 - 5);
+    printf("%d\n", 6 * 5);
+    printf("%f\n", 6.0 / 4);
+
+    // printf("%s",6==6);
+    int tk;
+    scanf("\n%d", &tk);
+    if (tk > 5000)
+    {
+        printf("Cox'bazar jabo");
+    }
+    else
+    {
+        printf("Jabo na");
+    }
+
+    return 0;
+}
